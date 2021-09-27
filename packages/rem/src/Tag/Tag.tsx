60c21e24c8da6019853f3afe4d1c985250e16662
melodyWxy <18210711176@163.com>
2021-09-27 09:46:20 +0800

    feat(rem): tag-sass

--- packages/rem/src/Tag/Tag.tsx
+++ packages/rem/src/Tag/Tag.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import cls from 'classnames';
 import PropTypes from 'prop-types';
-import { TagProps } from './type';
+import { TagProps, TagTypes, SizeTypes } from './type';
 
 import './Tag.scss';
 import { NativeProps } from '../global/type';
 
-
+const classPrefix = `rem-tag`;
 
 export const Tag =  React.forwardRef<HTMLDivElement, TagProps & NativeProps>(({
   className = '',
+  size = SizeTypes.medium,
+  mark = false,
+  plain = false,
+  round = false,
+  closeable = false,
+  type =  TagTypes.default,
+  style = {},
   ...rest
-}, ref)=>(
-  <div ref={ref} className={cls('wrap', className)} {...rest}/>
-));
+}, ref)=>{
+  
+  return (
+    <span ref={ref} className={cls(classPrefix, className)} {...rest}/>
+  );
+})
 
 Tag.propTypes = {
   className: PropTypes.string,
