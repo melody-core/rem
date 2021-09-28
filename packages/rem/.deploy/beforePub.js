9d72a2eec2fee404838a908c96d11c55fbb7ceee
六弦 <liuxian.wang@tuya.com>
2021-09-28 11:33:04 +0800

    feat(cd): fix-dist-html-name

--- packages/rem/.deploy/beforePub.js
+++ packages/rem/.deploy/beforePub.js
@@ -2,11 +2,14 @@
 const fs = require('fs');
 const path = require('path');
 
+const reg = /Storybook/g;
 
 const targetPath = path.resolve(__dirname, './../public/index.html');
-
 const zstr = fs.readFileSync(targetPath, 'utf-8');
-
 const res = zstr.replace("Storybook", "RemUI");
-
 fs.writeFileSync(targetPath, res, 'utf-8');
+
+const targetPathIframe = path.resolve(__dirname, './../public/iframe.html');
+const zstrFrame = fs.readFileSync(targetPathIframe, 'utf-8');
+const zstrFrameRes = zstrFrame.replace(reg, "RemUI");
+fs.writeFileSync(targetPathIframe, zstrFrameRes, 'utf-8');
