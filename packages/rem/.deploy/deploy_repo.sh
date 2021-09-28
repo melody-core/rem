5d59ad57429802337b3db3e7c282e6a25cd1a32d
六弦 <liuxian.wang@tuya.com>
2021-09-28 11:02:24 +0800

    feat(rem): repoCD+deploy

---
+++ packages/rem/.deploy/deploy_repo.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 进入生成的文件夹
+node packages/rem/.deploy/beforePub.js
+
+cd packages/rem/public
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/melody-core/rem.github.io.git master
